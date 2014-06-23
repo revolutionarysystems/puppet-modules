aba95b07618f9e672c280332a4a6d3007df888df
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-23 12:09:01 +0100

    fixed naming of nexus-artifact parts

--- nexus-artifact/manifests/init.pp
+++ nexus-artifact/manifests/init.pp
@@ -3,17 +3,17 @@ define nexus-artifact($url="", $repo="", $groupId="", $artifactId="", $version="
   $sourceName = "redirect?r=${repo}&g=${groupId}&a=${artifactId}&v=${version}&p=${type}"
   $escapedSourceName = "redirect\?r\=${repo}\&g\=${groupId}\&a\=${artifactId}\&v\=${version}\&p\=${type}"
   $destName = "${artifactId}.${type}"
-  exec { "nexus-wget":
+  exec { "nexus-wget-${artifactId}":
     require => File["/opt/puppet/artifacts/tmp"],
     command => "/usr/bin/wget -N '${url}/service/local/artifact/maven/$sourceName'",
     cwd => "/opt/puppet/artifacts/tmp",
   }
-  exec { "nexus-copy-artifact":
+  exec { "nexus-copy-artifact-${artifactId}":
     command => "/bin/cp ${escapedSourceName} ${destName}",
     cwd => "/opt/puppet/artifacts/tmp",
     subscribe => Exec["nexus-wget"]
   }
-  file { "nexus-artifact":
+  file { "/opt/puppet/artifacts/${destName}":
     path => "/opt/puppet/artifacts/${destName}",
     ensure => "present",
     source => "/opt/puppet/artifacts/tmp/${destName}",
