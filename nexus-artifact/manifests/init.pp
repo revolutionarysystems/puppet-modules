2a207fe297280ce602a860248d388fdcaa13ec96
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-23 12:13:01 +0100

    Fixed nexus dependencies

--- nexus-artifact/manifests/init.pp
+++ nexus-artifact/manifests/init.pp
@@ -11,12 +11,12 @@ define nexus-artifact($url="", $repo="", $groupId="", $artifactId="", $version="
   exec { "nexus-copy-artifact-${artifactId}":
     command => "/bin/cp ${escapedSourceName} ${destName}",
     cwd => "/opt/puppet/artifacts/tmp",
-    subscribe => Exec["nexus-wget"]
+    subscribe => Exec["nexus-wget-${artifactId}"]
   }
   file { "/opt/puppet/artifacts/${destName}":
     path => "/opt/puppet/artifacts/${destName}",
     ensure => "present",
     source => "/opt/puppet/artifacts/tmp/${destName}",
-    require => Exec["nexus-copy-artifact"]
+    require => Exec["nexus-copy-artifact-${artifactId}"]
   }
 }
