60663ae84c436634fe90989b2545025968042063
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-23 09:46:04 +0100

    fixed cloud-dashboard

--- cloud-dashboard/manifests/init.pp
+++ cloud-dashboard/manifests/init.pp
@@ -6,14 +6,14 @@ class cloud-dashboard ($version="LATEST", $serviceUrl="/cloud-service/", $deploy
     version => $version,
     type => "zip"
   }
-  file { "cloud-dashboard.war":
+  file { "cloud-dashboard.zip":
     require => Class["haven-artifact"],
     path => "/var/lib/tomcat7/webapps/${deployName}.war",
     ensure => "present",
-    source => "/opt/puppet/artifacts/cloud-dashboard.war"
+    source => "/opt/puppet/artifacts/cloud-dashboard.zip"
   }
   file { "config.js":
-    require => File["cloud-dashboard.war"],
+    require => File["cloud-dashboard.zip"],
     path => "/var/lib/tomcat7/webapps/${deployName}/js/config.js",
     ensure => "present",
     content => template("cloud-dashboard/config.js"),
