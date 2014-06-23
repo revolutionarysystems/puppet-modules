4e70d0cc052297e5bd75cb2b5180bc5d969eb5cd
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-23 09:58:01 +0100

    Reworked cloud-dashboard module to extract zip

--- cloud-dashboard/manifests/init.pp
+++ cloud-dashboard/manifests/init.pp
@@ -1,19 +1,28 @@
 class cloud-dashboard ($version="LATEST", $serviceUrl="/cloud-service/", $deployName="cloud-dashboard"){
   include tomcat7
+  file {"/opt/puppet/artifacts/${artifactId}":
+    ensure => "directory"
+  }
   class {"haven-artifact":
+    requires => File["/opt/puppet/artifacts/${artifactId}],
     url => "build.revsys.co.uk/haven-repository",
     artifactId => "cloud-dashboard",
     version => $version,
     type => "zip"
   }
-  file { "cloud-dashboard.zip":
+  archive { "/opt/puppet/artifacts/cloud-dashboard.zip":
     require => Class["haven-artifact"],
-    path => "/var/lib/tomcat7/webapps/${deployName}.war",
-    ensure => "present",
-    source => "/opt/puppet/artifacts/cloud-dashboard.zip"
+    ensure => unpacked,
+    cwd    => "/opt/puppet/artifacts/${artifactId}",
+  }
+  file { "/var/lib/tomcat7/webapps/${deployName}":
+    ensure => "directory",
+    recurse => true,
+    purge => true,
+    source => "/opt/puppet/artifacts/cloud-dashboard"
   }
   file { "config.js":
-    require => File["cloud-dashboard.zip"],
+    require => File[/var/lib/tomcat7/webapps/${deployName}],
     path => "/var/lib/tomcat7/webapps/${deployName}/js/config.js",
     ensure => "present",
     content => template("cloud-dashboard/config.js"),
