472f0ae48d14ba5451c7a0ac17f2380c1544f2af
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-23 14:37:10 +0100

    fixed typo

--- haven-repository/manifests/init.pp
+++ haven-repository/manifests/init.pp
@@ -1,7 +1,7 @@
 class haven-repository ($version="LATEST", $deploy_name="haven-repository", $cloud_container=""){
   class {"resource-repository":
     version => $version,
-    deploy-name => $deploy_name,
+    deploy_name => $deploy_name,
     cloud_container => $cloud_container
   }
 }
