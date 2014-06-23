5223a1a19fe03c1a42102b33b7285272648f79d7
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-23 12:16:55 +0100

    Corrected artifact id of resource repository

--- resource-repository/manifests/init.pp
+++ resource-repository/manifests/init.pp
@@ -1,18 +1,18 @@
 class resource-repository ($version="LATEST", $deployName="resource-repository"){
   class {"tomcat7": }
-  nexus-artifact::war{ "resource-repository.war":
+  nexus-artifact::war{ "resource-repository-service.war":
     url => "build.revsys.co.uk/nexus",
     repo => "snapshots",
     groupId => "uk.co.revsys.cloud",
-    artifactId => "resource-repository",
+    artifactId => "resource-repository-service",
     version => $version
   }
   file { "/var/lib/tomcat7/webapps/${deployName}":
-    require => Nexus-Artifact::War["resource-repository.war"],
+    require => Nexus-Artifact::War["resource-repository-service.war"],
     ensure => "directory",
     recurse => true,
     purge => true,
-    source => "/opt/puppet/artifacts/resource-repository"
+    source => "/opt/puppet/artifacts/resource-repository-service"
   }
   file { "/var/lib/tomcat7/webapps/${deployName}/WEB-INF/classes/resource-repository.properties":
     require => File["/var/lib/tomcat7/webapps/${deployName}"],
