6d33df6194c68b36d3fce746e2173a9b7583da36
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-24 12:34:54 +0100

    Added user-manager module

---
+++ tomcat-nexus-war/manifests/init.pp
@@ -0,0 +1,17 @@
+define tomcat-nexus-war ($nexus_url="", $nexus_repo="", $groupId="", $artifactId="", $version="LATEST", $deploy_name="") {
+	include tomcat7
+	nexus-artifact::war{ "${artifactId}.war":
+		url => $nexus_url,
+		repo => $nexus_repo,
+		groupId => $groupId,
+		artifactId => $artifactId,
+		version => $version
+	}
+	file { "/var/lib/tomcat7/webapps/${deploy_name}":
+		require => Nexus-Artifact::War["${artifactId}.war"],
+		ensure => "directory",
+		recurse => true,
+		purge => true,
+		source => "/opt/puppet/artifacts/${artifactId}"
+	}
+}
---
+++ user-manager/manifests/init.pp
@@ -0,0 +1,16 @@
+class user-manager ($version="LATEST", $deploy_name="user-manager", $db_host="localhost"){
+  tomcat-nexus-war{ "user-manager-service.war":
+    url => "build.revsys.co.uk/nexus",
+    repo => "snapshots",
+    groupId => "uk.co.revsys.user-manager",
+    artifactId => "user-manager-service",
+    version => $version,
+    deploy_name => $deploy_name
+  }
+  file { "/var/lib/tomcat7/webapps/${deploy_name}/WEB-INF/classes/user-manager.properties":
+    require => File["/var/lib/tomcat7/webapps/${deploy_name}"],
+    ensure => "present",
+    content => template("user-manager/user-manager.properties.erb"),
+    notify => Service["tomcat7"]
+  }
+}
