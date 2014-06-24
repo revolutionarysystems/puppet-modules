95a4baa675cbdcf6e4ad1510d54ae480732f94e2
Adam Knight <adam.knight@opensquare.co.uk>
2014-06-24 17:03:29 +0100

    fixed ui-framework module

--- ui-framework/manifests/init.pp
+++ ui-framework/manifests/init.pp
@@ -1,9 +1,9 @@
-class ui-framework ($version="LATEST", $deploy_name="ui-framework", $cloud_container="", $resources_path="", $user_manager_url=""){
-  tomcat-nexus-war{ "ui-framework-service.war":
+define ui-framework ($version="LATEST", $deploy_name="ui-framework", $cloud_container="", $resources_path="", $user_manager_url=""){
+  tomcat-nexus-war{ "ui-framework.war":
     nexus_url => "build.revsys.co.uk/nexus",
     nexus_repo => "snapshots",
-    groupId => "uk.co.revsys.ui-framework",
-    artifactId => "ui-framework-service",
+    groupId => "uk.co.revsys",
+    artifactId => "ui-framework",
     version => $version,
     deploy_name => $deploy_name
   }
