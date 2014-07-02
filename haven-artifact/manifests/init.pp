20a0c36860e40bfa7d2329cfe53a04602a0a1709
Adam Knight <adam.knight@opensquare.co.uk>
2014-07-02 09:14:56 +0100

    Add tar function to haven artifcat module

--- haven-artifact/manifests/init.pp
+++ haven-artifact/manifests/init.pp
@@ -1,8 +1,34 @@
 define haven-artifact ($url="", $artifactId="", $version="", $type="") {
+  haven-artifact::file { "haven-artifact-file-${artifactId}":
+    url => $url,
+    artifactId => $artifactId,
+    version => $version,
+    $file => "${artifactId}.${type}"
+  }
+}
+
+define haven-artifact::file ($url="", $artifactId="", $version="", $file="") {
   include puppet-artifacts
   exec { "haven-wget-${artifactId}":
     require => File["/opt/puppet/artifacts/"],
-    command => "/usr/bin/wget -N '${url}/${artifactId}/${version}/artifact/${artifactId}.${type}'",
+    command => "/usr/bin/wget -N '${url}/${artifactId}/${version}/artifact/${file}'",
     cwd => "/opt/puppet/artifacts/",
+  }
+}
+
+define haven-artifact::tar ($url="", $artifactId="", $version="", $file="") {
+  file {"/opt/puppet/artifacts/${file}": 
+    ensure => directory
+  }
+  haven-artifact::file { "haven-artifact-file-${file}":
+    url => $url,
+    artifactId => $artifactId,
+    version => $version,
+    $file => "${file}.tar.gz"
+  }
+  exec { "extract-${file}":
+    require => [File["/opt/puppet/artifacts/${file}"], Haven-Artifact::File["haven-artifact-file-${file}"]],
+    command => "/bin/tar -zxvf ../${file}.tar.gz",
+    cwd => "/opt/puppet/artifacts/${file}",
   }
 }
