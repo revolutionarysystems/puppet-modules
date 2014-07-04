8ce0cd93b810c15e785b26366d29ca6c3b82562b
Adam Knight <adam.knight@opensquare.co.uk>
2014-07-04 12:20:28 +0100

    Added oddball module

---
+++ oddball/manifests/init.pp
@@ -0,0 +1,17 @@
+define oddball ($version="", $deploy_name="oddball", $resource_container="", $consumers="", $kinesis_application_name="", $kinesis_stream=""){
+  include tomcat7
+  tomcat-nexus-war{ "oddball-service.war":
+    nexus_url => "build.revsys.co.uk/nexus",
+    nexus_repo => "snapshots",
+    groupId => "uk.co.revsys.oddball",
+    artifactId => "oddball-service",
+    version => $version,
+    deploy_name => $deploy_name
+  }
+  file { "/var/lib/tomcat7/webapps/${deploy_name}/WEB-INF/classes/oddball.properties":
+    require => Tomcat-Nexus-War["oddball-service.war"],
+    ensure => "present",
+    content => template("oddball/oddball.properties.erb"),
+    notify => Service["tomcat7"]
+  }
+}
