99cbccbc933c14363d23c08ef313e974108f09e0
Adam Knight <adam.knight@opensquare.co.uk>
2014-08-04 16:43:59 +0100

    corrected variable assignment

--- subscription-manager/manifests/init.pp
+++ subscription-manager/manifests/init.pp
@@ -1,9 +1,9 @@
 define subscription-manager ($version="LATEST", $deploy_name="subscription-manager", $db_host="localhost", $db_name="subscription-manager", $templates_version="LATEST", $instance_container="", $instance_path="", $user_manager_db_host="", $security="default", $security_administrator_role="subscription-manager:administrator"){
 
-  $template_repository_type => "cloud"
-  $template_container => "revsys-haven-artifacts"
-  $template_path => "subscription-manager-templates/${templates_version}/artifact"
-  $instance_repository_type => "cloud"
+  $template_repository_type = "cloud"
+  $template_container = "revsys-haven-artifacts"
+  $template_path = "subscription-manager-templates/${templates_version}/artifact"
+  $instance_repository_type = "cloud"
 
   tomcat-nexus-war{ "subscription-manager-webapp.war":
     nexus_url => "build.revsys.co.uk/nexus",
