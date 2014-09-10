f728a3f894e912535240846d4d76d050f13fcdfc
Adam Knight <adam.knight@opensquare.co.uk>
2014-09-10 16:01:57 +0100

    Set view repo properties for subscription manager module

--- subscription-manager/manifests/init.pp
+++ subscription-manager/manifests/init.pp
@@ -1,8 +1,11 @@
-define subscription-manager ($version="LATEST", $deploy_name="subscription-manager", $db_host="localhost", $db_name="subscription-manager", $templates_version="LATEST", $instance_container="", $instance_path="", $user_manager_db_host="", $security="default", $security_administrator_role="subscription-manager:administrator"){
+define subscription-manager ($version="LATEST", $deploy_name="subscription-manager", $db_host="localhost", $db_name="subscription-manager", $templates_version="LATEST", $views_version="LATEST", $instance_container="", $instance_path="", $user_manager_db_host="", $security="default", $security_administrator_role="subscription-manager:administrator"){
 
   $template_repository_type = "cloud"
   $template_container = "revsys-haven-artifacts"
   $template_path = "subscription-manager-templates/${templates_version}/artifact"
+  $view_repository_type = "cloud"
+  $view_container = "revsys-haven-artifacts"
+  $view_path = "subscription-manager-views/${views_version}/artifact"
   $instance_repository_type = "cloud"
 
   tomcat-nexus-war{ "subscription-manager-webapp.war":
