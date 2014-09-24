2675957fad47e87cbd6d13f73e000c3e6880bf71
Adam Knight <adam.knight@opensquare.co.uk>
2014-09-24 10:48:49 +0100

    Added help-repository.properties.erb

--- help-repository/manifests/init.pp
+++ help-repository/manifests/init.pp
@@ -1,4 +1,4 @@
-define help-repository ($version="", $deploy_name="help-repository"){
+define help-repository ($version="", $deploy_name="help-repository", $security="disabled", $user_manager_db_host="localhost", $security_administrator_role="administrator"){
   include tomcat7
   tomcat-nexus-war{ "help-repository-service.war":
     nexus_url => "build.revsys.co.uk/nexus",
@@ -7,5 +7,11 @@ define help-repository ($version="", $deploy_name="help-repository"){
     artifactId => "help-repository-service",
     version => $version,
     deploy_name => $deploy_name
+  }
+  file { "/var/lib/tomcat7/webapps/${deploy_name}/WEB-INF/classes/help-repository.properties":
+    require => File["/var/lib/tomcat7/webapps/${deploy_name}"],
+    ensure => "present",
+    content => template("help-repository/help-repository.properties.erb"),
+    notify => Service["tomcat7"]
   }
 }
