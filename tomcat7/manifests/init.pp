aedd3a9c800239ce3411a00806d5e72f0ed46edf
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-03 16:57:10 +0000

    Added tomcat7 settings

--- tomcat7/manifests/init.pp
+++ tomcat7/manifests/init.pp
@@ -1,4 +1,4 @@
-class tomcat7 {
+define tomcat7 ($max_heap_size="512m", $permgen_size="32m", $max_permgen_size="64m"){
   package { "tomcat7":
     ensure => present,
     before => Service["tomcat7"]
@@ -7,5 +7,11 @@ class tomcat7 {
   service { "tomcat7":
     ensure => running,
     enable => true
+  }
+  
+  file { "/etc/default/tomcat7":
+    ensure => "present",
+    content => template("tomcat7/tomcat7.erb"),
+    notify => Service["tomcat7"]
   }
 }
