6646019d9df2eb11581252f96e900db015e03288
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-17 09:46:34 +0000

    Allow specific cloud credentials

--- objectology/manifests/init.pp
+++ objectology/manifests/init.pp
@@ -1,4 +1,4 @@
-define objectology ($version="LATEST", $deploy_name="objectology", $db_host="localhost", $db_name="objectology", $template_repository_type="cloud", $template_container="", $template_path="/", $instance_repository_type="cloud", $instance_container="", $instance_path="/", $view_repository_type="cloud", $view_container="", $view_path="/", $security="default", $user_manager_db_host="", $security_administrator_role=""){
+define objectology ($version="LATEST", $deploy_name="objectology", $db_host="localhost", $db_name="objectology", $cloud_identity="", $cloud_credential="", $template_repository_type="cloud", $template_container="", $template_path="/", $instance_repository_type="cloud", $instance_container="", $instance_path="/", $view_repository_type="cloud", $view_container="", $view_path="/", $security="default", $user_manager_db_host="", $security_administrator_role=""){
   tomcat-nexus-war{ "objectology-webapp.war":
     nexus_url => "build.revsys.co.uk/nexus",
     nexus_repo => "snapshots",
