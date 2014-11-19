673d469ec405b1ab68be3dae1c3cabcda8cc4da5
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-19 13:37:06 +0000

    Added ami scripts module

---
+++ ami-scripts/manifests/init.pp
@@ -0,0 +1,20 @@
+define ami-scripts($version="LATEST"){
+
+    haven-artifact::tar { "ami-scripts.tar.gz":
+      url => "http://build.revsys.com/haven-repository",
+      artifactId => "ami-scripts",
+      version => $version,
+      file => "ami-scripts"
+    }
+    
+    file { "/root/ami-scripts":
+      require => Haven-Artifact::Tar["ami-scripts.tar.gz"],
+      ensure => "directory",
+      recurse => true,
+      owner => "root",
+      group => "root",
+      mode => 755
+      source => "/opt/puppet/artifacts/ami-scripts",
+    }
+
+}
