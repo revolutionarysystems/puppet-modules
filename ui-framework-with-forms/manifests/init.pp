aa98047a82487e6c7b3d26d7ba709cff97b86b3d
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-21 16:59:37 +0000

    Added Rhinoforms.properties

--- ui-framework-with-forms/manifests/init.pp
+++ ui-framework-with-forms/manifests/init.pp
@@ -1,4 +1,4 @@
-define ui-framework-with-forms ($version="LATEST", $repo="releases", $deploy_name="ui-framework-with-forms", $repository_type="cloud", $resources_container="", $resources_path="", $cloud_identity="", $cloud_credential="", $security_filters="none", $user_manager_url="", $user_manager_db_host="", $login_url="/login.html", $login_success_url="index.html"){
+define ui-framework-with-forms ($version="LATEST", $repo="releases", $deploy_name="ui-framework-with-forms", $repository_type="cloud", $resources_container="", $resources_path="", $cloud_identity="", $cloud_credential="", $security_filters="none", $user_manager_url="", $user_manager_db_host="", $login_url="/login.html", $login_success_url="index.html", $show_forms_debug_bar="false"){
   tomcat-nexus-war{ "ui-framework-with-forms.war":
     nexus_url => "build.revsys.co.uk/nexus",
     nexus_repo => $repo,
@@ -12,6 +12,12 @@ define ui-framework-with-forms ($version="LATEST", $repo="releases", $deploy_nam
     require => File["/var/lib/tomcat7/webapps/${deploy_name}"],
     ensure => "present",
     content => template("ui-framework-with-forms/ui.properties.erb"),
+    notify => Service["tomcat7"]
+  }
+  file { "/var/lib/tomcat7/webapps/${deploy_name}/WEB-INF/classes/Rhinoforms.properties":
+    require => File["/var/lib/tomcat7/webapps/${deploy_name}"],
+    ensure => "present",
+    content => template("ui-framework-with-forms/Rhinoforms.properties.erb"),
     notify => Service["tomcat7"]
   }
 }
