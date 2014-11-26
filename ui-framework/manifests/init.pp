302452e34b9ec2e1d4453eb424a67655b35ac062
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-26 10:58:54 +0000

    fixed typo

--- ui-framework/manifests/init.pp
+++ ui-framework/manifests/init.pp
@@ -1,5 +1,5 @@
 define ui-framework ($version="LATEST", $repo="releases", $deploy_name="ui-framework", $repository_type="cloud", $resources_container="", $resources_path="", $security_filters="none", $user_manager_url="", $user_manager_db_host="localhost", $cloud_identity="", $cloud_credential="", $login_url="/login.html", $login_success_url="index.html"){
-  tomcat-nexus-war{ "ui-framework-${deployName}.war":
+  tomcat-nexus-war{ "ui-framework-${deploy_name}.war":
     nexus_url => "build.revsys.co.uk/nexus",
     nexus_repo => $repo,
     groupId => "uk.co.revsys",
