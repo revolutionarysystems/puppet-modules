2d20af4a7536139fe67e6b914ac41ea415e74723
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-26 11:00:10 +0000

    Allow multiple declarations

--- tomcat-nexus-war/manifests/init.pp
+++ tomcat-nexus-war/manifests/init.pp
@@ -1,5 +1,5 @@
 define tomcat-nexus-war ($nexus_url="", $nexus_repo="", $groupId="", $artifactId="", $version="LATEST", $deploy_name="", $purge="true") {
-	nexus-artifact::war{ "${artifactId}.war":
+	nexus-artifact::war{ "${artifactId}-${deploy_name}.war":
 		url => $nexus_url,
 		repo => $nexus_repo,
 		groupId => $groupId,
@@ -7,7 +7,7 @@ define tomcat-nexus-war ($nexus_url="", $nexus_repo="", $groupId="", $artifactId
 		version => $version
 	}
 	file { "/var/lib/tomcat7/webapps/${deploy_name}":
-		require => Nexus-Artifact::War["${artifactId}.war"],
+		require => Nexus-Artifact::War["${artifactId}-${deploy_name}.war"],
 		ensure => "directory",
         group => "tomcat7",
         owner => "tomcat7",
