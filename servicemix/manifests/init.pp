f688f68f32776ffd5e000d866de9d8e5cb74aac6
Adam Knight <adam@revolutionarysystems.co.uk>
2014-12-01 12:09:53 +0000

    corrected symlink

--- servicemix/manifests/init.pp
+++ servicemix/manifests/init.pp
@@ -84,7 +84,7 @@ class servicemix ($features="", $repo="releases", $dependencies_version="LATEST"
     mode => 755,
     source => "/opt/puppet/artifacts/esb-dependencies"
   }
-  file { '/opt/servicemix/routes':
+  file { '/opt/apache-servicemix-4.5.2/routes':
       ensure => 'link',
       target => '/opt/apache-servicemix-4.5.2/deploy'
     }
