c6f916412de13be76a51935af8fbae300f6d912a
Adam Knight <adam@revolutionarysystems.co.uk>
2014-12-08 15:52:07 +0000

    Added chat app module

---
+++ chat-app/manifests/init.pp
@@ -0,0 +1,27 @@
+class chat-app ($version="LATEST") {
+
+  include forever
+  
+  haven-artifact::tar { "chat-app.tar.gz": 
+    url => "build.revsys.co.uk/haven-repository",
+    artifactId => "chat-app",
+    version => $version,
+    file => "chat-app"
+  }
+  
+  file { "/opt/chat-app":
+    require => Haven-Artifact::Tar["chat-app.tar.gz"],
+    ensure => "directory",
+    recurse => true,
+    purge => true,
+    source => "/opt/puppet/artifacts/chat-app"
+  }
+  
+  exec { "run_chat":
+    require => File['/opt/chat-app'],
+    cwd => "/opt/chat-app",
+    command => "forever start --uid chat-app -a -w server.js",
+    onlyif => "forever list | grep chat-app | wc -l",
+    path => ["/bin", "/usr/bin", "/usr/local/bin"]
+  }
+}
