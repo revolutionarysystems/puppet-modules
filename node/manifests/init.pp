d802e774665cc76330200b461f0d1a3ddd10144e
Adam Knight <adam@revolutionarysystems.co.uk>
2014-12-08 14:45:22 +0000

    Added node module

---
+++ node/manifests/init.pp
@@ -0,0 +1,15 @@
+define node (){
+  package {'nodejs':
+    ensure => present
+  }
+  
+  package{'npm':
+    ensure => 'present'
+  }
+  
+  file { '/usr/bin/node':
+    require => Package['nodejs'],
+    ensure => 'link',
+    target => '/usr/bin/nodejs'
+  }
+}
