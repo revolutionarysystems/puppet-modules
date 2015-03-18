bcce7d8d522c698142e6027da621700a38a627a2
Adam Knight <adam@revolutionarysystems.co.uk>
2015-03-18 15:23:45 +0000

    Added security params to haven repo

--- haven-repository/manifests/init.pp
+++ haven-repository/manifests/init.pp
@@ -1,8 +1,11 @@
-class haven-repository ($version="LATEST", $deploy_name="haven-repository", $repository_type="cloud", $resource_container=""){
+class haven-repository ($version="LATEST", $deploy_name="haven-repository", $repository_type="cloud", $resource_container="", $security="disabled", $security_role="", $user_manager_db_host=""){
   class {"resource-repository":
     version => $version,
     deploy_name => $deploy_name,
     repository_type => $repository_type,
     resource_container => $resource_container
+    security => $security,
+    security_role => $security_role,
+    user_manager_db_host => $user_manager_db_host
   }
 }
