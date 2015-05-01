ff0e3a5e4cb414e0afbb1d02821509ede8470cc4
Adam Knight <adam@revolutionarysystems.co.uk>
2015-05-01 15:09:38 +0100

    Allow multiple deployments of ui with forms

--- ui-framework-with-forms/manifests/init.pp
+++ ui-framework-with-forms/manifests/init.pp
@@ -1,5 +1,5 @@
 define ui-framework-with-forms ($version="LATEST", $repo="releases", $deploy_name="ui-framework-with-forms", $repository_type="cloud", $resources_container="", $resources_path="", $cloud_identity="", $cloud_credential="", $security_filters="none", $user_manager_url="", $user_manager_db_host="", $login_url="/login.html", $login_success_url="index.html", $show_forms_debug_bar="false"){
-  tomcat-nexus-war{ "ui-framework-with-forms.war":
+  tomcat-nexus-war{ "ui-framework-with-forms-${deploy_name}.war":
     nexus_url => "build.revsys.co.uk/nexus",
     nexus_repo => $repo,
     groupId => "uk.co.revsys",
