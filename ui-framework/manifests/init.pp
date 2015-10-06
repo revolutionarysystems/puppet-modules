87981e0e70934e86f570dd01373354c630faf59e
Adam Knight <adam@revolutionarysystems.co.uk>
2015-10-06 17:09:56 +0100

    added account shield parameters

--- ui-framework/manifests/init.pp
+++ ui-framework/manifests/init.pp
@@ -1,4 +1,4 @@
-define ui-framework ($version="LATEST", $repo="releases", $deploy_name="ui-framework", $repository_type="cloud", $resources_container="", $resources_path="", $security_filters="none", $user_manager_url="", $user_manager_db_host="localhost", $cloud_identity="", $cloud_credential="", $login_url="/login.html", $login_success_url="index.html"){
+define ui-framework ($version="LATEST", $repo="releases", $deploy_name="ui-framework", $repository_type="cloud", $resources_container="", $resources_path="", $security_filters="none", $user_manager_url="", $user_manager_db_host="localhost", $cloud_identity="", $cloud_credential="", $login_url="/login.html", $login_success_url="index.html", $account_shield_url="", $account_shield_account="", $account_shield_username="", $account_shield_password=""){
   tomcat-nexus-war{ "ui-framework-${deploy_name}.war":
     nexus_url => "build.revsys.co.uk/nexus",
     nexus_repo => $repo,
